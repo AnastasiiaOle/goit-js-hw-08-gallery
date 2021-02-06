2975ed21b00a09427ded0782e1894f97490d60b5
Anastasiia Oleinikova <anastasiiaole@gmail.com>
2021-02-06 21:54:20 +0100

    updated

--- js-task.js
+++ js-task.js
@@ -43,8 +43,8 @@ function onGalleryClick(event) {
     if (event.target.nodeName === 'IMG') {
     
         refs.lightbox.classList.add('is-open');
-        refs.lightbox__image.src = event.target.getAttribute("data-source");
-        refs.lightbox__image.alt = event.target.alt;
+        refs.lightbox.querySelector('.lightbox__image').src = event.target.dataset.source;
+        refs.lightbox.querySelector('.lightbox__image').alt = event.target.alt;
     }
     window.addEventListener('keyup', clickKey);
 }
@@ -54,8 +54,8 @@ refs.lightboxBtn.addEventListener('click', onCloseClick);
 function onCloseClick(event) {
     event.preventDefault();
     refs.lightbox.classList.remove('is-open');
-    refs.lightbox___img.src = '';
-    refs.lightbox___img.alt = '';
+    refs.lightbox.querySelector('.lightbox__image').src = '';
+    refs.lightbox.querySelector('.lightbox__image').alt = '';
     window.removeEventListener('keyup', clickKey );
 
 }
